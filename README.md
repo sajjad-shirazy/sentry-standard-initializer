16805c7e837c5e90479492e89f41e62f8493d7db
Sajjad Shirazy <shirazy.sajjad@gmail.com>
2020-12-02 11:53:12 +0330

    feat: initial implementation

--- README.md
+++ README.md
@@ -1,11 +1,19 @@
-# Sentry Standard Initializaer
+# Sentry Standard Setup
 
-[![npm version](https://badge.fury.io/js/sentry-standard-initializer.svg)](https://www.npmjs.com/package/sentry-standard-initializer)
+[![npm version](https://badge.fury.io/js/standard-sentry-setup.svg)](https://www.npmjs.com/package/standard-sentry-setup)
 
 Initializes Sentry in a standard way using environment values.
+
+## Features
+
+- Express integration
+- HTTP requests tracing
+- Excluding Prometheus requests
+- Handling environments
+- Handling Sentry releases feature using Git short SHA key
 
 ## Install
 
 ```bash
-npm i sentry-standard-initializer
+npm i standard-sentry-setup
 ```
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,73 @@
-export function getHello(): string {
-  return 'Hello from the new package!';
+import * as Sentry from '@sentry/node';
+import { Integrations } from '@sentry/tracing';
+import { Router } from 'express';
+import { IncomingMessage, ServerResponse } from 'http';
+
+const {
+  NODE_ENV,
+  RELEASE_GIT_SHORT_SHA,
+  SENTRY_DSN,
+  SENTRY_LOG_LEVEL,
+  SENTRY_TRACES_SAMPLE_RATE,
+} = process.env;
+
+function filterMetricsRequests(tracingHandler: Function): any {
+  return (
+    req: IncomingMessage,
+    res: ServerResponse,
+    next: (error?: any) => void,
+  ) => {
+    // skipping requests to /metrics
+    if (req.url == '/metrics') {
+      next();
+    } else {
+      tracingHandler(req, res, next);
+    }
+  };
+}
+
+export function generateOptions(app?: Router) {
+  return {
+    dsn: SENTRY_DSN,
+    environment: NODE_ENV,
+    release: RELEASE_GIT_SHORT_SHA,
+    logLevel: parseInt(SENTRY_LOG_LEVEL),
+    integrations: integrateWithRouter(app),
+    // Be sure to lower this in production
+    tracesSampleRate: parseFloat(SENTRY_TRACES_SAMPLE_RATE),
+  };
+}
+
+export function init(app?: Router) {
+  Sentry.init(generateOptions(app));
+}
+
+function integrateWithRouter(app: Router) {
+  const integrations = [];
+  if (app) {
+    // RequestHandler creates a separate execution context using domains, so that every
+    // transaction/span/breadcrumb is attached to its own Hub instance
+    app.use(Sentry.Handlers.requestHandler({ ip: true }));
+    // TracingHandler creates a trace for every incoming request
+    app.use(filterMetricsRequests(Sentry.Handlers.tracingHandler()));
+    // the rest of your app
+    app.use(Sentry.Handlers.errorHandler());
+    integrations.push(
+      // enable HTTP calls tracing
+      new Sentry.Integrations.Http({ tracing: true }),
+      // enable Express.js middleware tracing
+      new Integrations.Express({ app }),
+    );
+  }
+  return integrations;
+}
+
+export function getConfigs() {
+  return {
+    NODE_ENV,
+    RELEASE_GIT_SHORT_SHA,
+    SENTRY_DSN,
+    SENTRY_LOG_LEVEL,
+    SENTRY_TRACES_SAMPLE_RATE,
+  };
 }
